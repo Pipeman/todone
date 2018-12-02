dc1b622cc06eaf698e1e89a6c196c6ce5b106e14
Pipeman <tozzy@DESKTOP-EQN2BPQ.localdomain>
2018-12-02 17:40:34 +0000

    (feat): First commit

---
+++ src/components/GroupCheckbox.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <b-checkbox
+      v-for="task in tasks"
+      v-model="checkedTask"
+      :key="task.id"
+    >{{ task.description }}</b-checkbox>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ToDo',
+  props: {
+    tasks: {
+      type: Array,
+      default: () => [],
+      required: true,
+    },
+  },
+  data () {
+    return {
+      checkedTasks: [],
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/ToDo.vue
@@ -0,0 +1,14 @@
+<template>
+  <b-checkbox></b-checkbox>
+</template>
+
+<script>
+export default {
+  name: 'ToDo'
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
